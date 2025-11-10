7ab00fb006abed53699278715f80aa023cab5ec7
PichetDotK <pckjoke@gmail.com>
2025-11-10 16:07:49 +0700

    update forms

--- src/app/views/pages/receive/receive.component.ts
+++ src/app/views/pages/receive/receive.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbCalendar, NgbDate, NgbDateParserFormatter, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-receive',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbDatepickerModule
+  ],
   templateUrl: './receive.component.html',
   styleUrl: './receive.component.scss'
 })
 export class ReceiveComponent {
-
+  selectedDate: NgbDateStruct;
 }
