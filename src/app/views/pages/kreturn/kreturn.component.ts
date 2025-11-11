04f9b871f32d300e3e03dc023a51d8f028698500
PichetDotK <pckjoke@gmail.com>
2025-11-11 16:53:19 +0700

    up pages

--- src/app/views/pages/kreturn/kreturn.component.ts
+++ src/app/views/pages/kreturn/kreturn.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-kreturn',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbModule
+  ],
   templateUrl: './kreturn.component.html',
   styleUrl: './kreturn.component.scss'
 })
 export class KreturnComponent {
+  private names = ['pichet', 'jirapan', 'sarosa', 'Blueberry', 'Cherry', 'Coconut', 'Grape', 'Grapefruit', 'Kiwi', 'Mango', 'Melon', 'Orange', 'Papaya', 'Pineapple', 'Strawberry'];
 
+  // ฟังก์ชัน search ที่ ngbTypeahead ต้องการ: รับ observable ของ input และคืน observable ของ suggestions
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term.length < 1 ? []
+        : this.names.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) !== -1).slice(0, 10))
+    );
+
+  // ถ้าต้องการแสดงผลใน input ต่างจากผลที่ถูกเลือก ให้ใช้ formatter
+  formatter = (result: string) => result;
 }
