c0d460577d1b708b4e9c3df506a6ca99b80341e0
PichetDotK <146558868+pichet1234@users.noreply.github.com>
2025-11-27 07:14:57 +0700

    Update uploadfile.service.ts

--- src/app/core/services/uploadfile.service.ts
+++ src/app/core/services/uploadfile.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Karupan } from '../../views/models/karupan.model';
 
 @Injectable({
   providedIn: 'root'
