91b29fe705133c5c0c65da3c78d4adbdaefb0f3a
PichetDotK <146558868+pichet1234@users.noreply.github.com>
2025-11-27 07:06:18 +0700

    update file interface karupan

---
+++ src/app/views/models/karupan.model.ts
@@ -0,0 +1,12 @@
+export interface Karupan {
+    kname: string;
+    karupantype: string;
+    detail: string;
+    price: number;
+    station: string;
+    expenditure: string;
+    usefullife: string;
+    status:string;
+    brand: string;
+    imageUrl: string;
+}
