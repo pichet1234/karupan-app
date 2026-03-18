ca74b0cc760fdcbb08247861b1481d85cb02082a
PichetDotK <146558868+pichet1234@users.noreply.github.com>
2026-03-18 20:45:06 +0700

    ห

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -6,7 +6,8 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   const authService = inject(AuthService);
   const token = authService.getAccessToken();
-
+console.log('TOKEN:', token);
+console.log('URL:', req.url);
   if (token) {
     const cloned = req.clone({
       setHeaders: {
